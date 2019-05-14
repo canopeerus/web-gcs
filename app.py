8fbdc44e6442ee2a244992b01939bea4e903fe67
Aditya Visvanathan <visvanathannaditya@gmail.com>
2019-05-14 22:31:51 +0530

    add fonts and signup page

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ def homepage ():
     
 # Main GCS Page route, redirects to 'gcslogin' if not logged in
 @app.route ("/gcsportal",methods=['GET','POST'])
-def gcshome():
+def gcs_home():
     if not session.get ('gcs_logged_in'):
         return redirect("/gcslogin",code=302)
     else:
@@ -18,16 +18,25 @@ def gcshome():
 
 # GCS Login page route
 @app.route ("/gcslogin")
-def gcslogin ():
+def gcs_login ():
     if not session.get ('gcs_logged_in'):
         return render_template ("gcs_login.html")
 
-
 # GCS Login form action route, accepts only POST requests
 @app.route ("/gcsloginform",methods=['POST'])
-def gcsloginaction ():
+def gcs_login_action ():
     session['gcs_logged_in'] = True
     return redirect("/gcsportal",code=302)
+
+# GCS Sign up page route
+@app.route ("/gcssignup")
+def gcs_signup ():
+    return render_template ("gcs_signup.html")
+
+# GCS Sign up page form action route
+@app.route ("/gcssignupform",methods=['POST'])
+def gcs_signup_action ():
+    return "Registered"
 
 if __name__ == "__main__":
     app.run (debug = True)
