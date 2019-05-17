c23a1cf01eeede1b04e2882abc0064e5654c3619
Aditya Visvanathan <visvanathannaditya@gmail.com>
2019-05-17 22:54:08 +0530

    add profile edit and login/signup error handling

--- app.py
+++ app.py
@@ -51,6 +51,8 @@ def gcs_login_action ():
     usernameval = request.form['username']
     pwval = request.form['password']
     qresult = GCSUser.query.filter_by (username=usernameval).first()
+    if qresult is None:
+        return render_template ("gcsloginerr.html")
     qpassword = qresult.password
     qsalt = qresult.salt
     if verify_password (qpassword,pwval,qsalt):
@@ -58,16 +60,17 @@ def gcs_login_action ():
         session['gcs_user'] = usernameval
         return redirect("/gcsportal",code=302)
     else:
-        return "Incorrect password"
+        return render_template ("gcsloginerr.html")
 
 # show user profile and account settings
-@app.route ("/gcsuserprofile",methods=['POST'])
+@app.route ("/gcsuserprofile",methods=['POST','GET'])
 def show_userprofile():
-    if session['gcs_logged_in']:
-        user = session['gcs_user']
-        qresult = GCSUser.query.filter_by(username=user).first()
-        return render_template ("gcsprofile.html",username = qresult.username, firstname = qresult.firstname, 
-                lastname = qresult.lastname,email_id = qresult.email_id)
+    if 'gcs_logged_in' in session:
+        if session['gcs_logged_in']:
+            user = session['gcs_user']
+            qresult = GCSUser.query.filter_by(username=user).first()
+            return render_template ("gcsprofile.html",username = qresult.username, firstname = qresult.firstname, lastname = qresult.lastname,email_id = qresult.email_id)
+
     else:
         return redirect ("/gcslogin",code=302)
 
@@ -89,12 +92,35 @@ def gcs_signup ():
 def gcs_signup_action ():
     salt = uuid.uuid4().hex
     pwd_hash = hash_password (request.form['password'],salt)
-    gcsuser_instance = GCSUser (username = request.form['username'],password = pwd_hash,
+    match = GCSUser.query.filter_by (username = request.form['username']).first()
+    if match is None:
+        gcsuser_instance = GCSUser (username = request.form['username'],password = pwd_hash,
             salt = salt, firstname = request.form['firstname'], lastname = request.form['lastname'], 
             email_id = request.form['email_id'])
-    db.session.add (gcsuser_instance)
-    db.session.commit ()
-    return "Registered"
+        db.session.add (gcsuser_instance)
+        db.session.commit ()
+        return redirect ("/gcslogin",code=302)
+    else:
+        return render_template ("gcs_signup_err.html")
+
+
+@app.route ("/gcsprofileedit",methods=['POST'])
+def gcs_profile_edit ():
+    if 'gcs_user' not in session:
+        return redirect ("/gcslogin",code=302)
+    user = session['gcs_user']
+    user_instance = GCSUser.query.filter_by (username = user).first()
+    if 'firstname' in request.args:
+        n_fname = request.args['firstname']
+        user_instance.firstname = n_fname
+    if 'lastname' in request.args:
+        n_lname = request.args['lastname']
+        user_instance.lastname = n_lname
+    if 'email' in request.args:
+        n_email = request.args['email']
+        user_instance.email_id = n_email
+    db.session.commit()
+    return redirect ("/gcsuserprofile",code=302)
 
 if __name__ == "__main__":
     app.run (debug = True)
